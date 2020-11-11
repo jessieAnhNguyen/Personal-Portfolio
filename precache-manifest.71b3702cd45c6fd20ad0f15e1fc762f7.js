16c6c64792b97baa2a0e22460410eac670ff1f28
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2020-11-11 11:33:08 +0700

    Updates

---
+++ precache-manifest.71b3702cd45c6fd20ad0f15e1fc762f7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b361795f62bab08433c2c109f92d593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6871438c15ff655f4724",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "b1b2260010a71397ab53",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "7435e0386fcae1042149",
+    "url": "/static/css/main.33a51dd1.chunk.css"
+  },
+  {
+    "revision": "22dfd917f9d62610b57c",
+    "url": "/static/js/2.3b5078a7.chunk.js"
+  },
+  {
+    "revision": "6871438c15ff655f4724",
+    "url": "/static/js/3.64c99ea7.chunk.js"
+  },
+  {
+    "revision": "b1b2260010a71397ab53",
+    "url": "/static/js/4.c014f258.chunk.js"
+  },
+  {
+    "revision": "7435e0386fcae1042149",
+    "url": "/static/js/main.4f7cf8aa.chunk.js"
+  },
+  {
+    "revision": "25e8781ded727375d461",
+    "url": "/static/js/runtime-main.51c00361.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "a06440f407802f1fceaef155379bae09",
+    "url": "/static/media/CSUGLogo.a06440f4.png"
+  },
+  {
+    "revision": "7c00141ffc58c671ffeb01396a2031b4",
+    "url": "/static/media/DHPhoto.7c00141f.jpg"
+  },
+  {
+    "revision": "38fe49793ca503a8e44ac7d2c5303ffb",
+    "url": "/static/media/DSCLogo.38fe4979.png"
+  },
+  {
+    "revision": "1f21d10acd4472712b0cf1804853828e",
+    "url": "/static/media/DSCPhoto.1f21d10a.jpg"
+  },
+  {
+    "revision": "008c62da4409844f69692d868edeec6c",
+    "url": "/static/media/DSCPhoto2.008c62da.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "849a51c66d6415410c53a13908e2d4f3",
+    "url": "/static/media/URLogo.849a51c6.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "336a56a040eccc9f40bc70724191120d",
+    "url": "/static/media/fptLogo.336a56a0.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "74c84136adde747e29beac358b944299",
+    "url": "/static/media/greetingPhoto.74c84136.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "384462cb8d07f06bbb1e5ff2b3440307",
+    "url": "/static/media/myPhoto2.384462cb.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "88296c7f8d969cab0db7e52af222f4c0",
+    "url": "/static/media/workPhoto.88296c7f.jpg"
+  }
+]);
