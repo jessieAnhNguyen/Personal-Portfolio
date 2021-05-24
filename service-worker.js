573fe783a8c4a2251e10fdb1c6d272dd87440cf9
jessieAnhNguyen <47696418+jessieAnhNguyen@users.noreply.github.com>
2021-05-24 03:16:16 +0000

    Deploying to gh-pages from  @ eeae6e04b477bb9065c38ef9f40c845faa0721a7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46bd6e64c212a8be197d3e84c9c3fee6.js"
+  "/precache-manifest.61314d118dae2098bca75f2d8675685a.js"
 );
 
 self.addEventListener('message', (event) => {
