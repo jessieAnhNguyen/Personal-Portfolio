1bffa5e9dcf569889a0bd8af91217cd64a6dc913
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-07-29 22:27:49 -0400

    Updates

---
+++ precache-manifest.51bfee8f3a0572bd81dace870a5ff8f5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b8e8f1736ec3df6b8c5cb050b90595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a2b3ce8e4bdd75e665",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "ea8b20392635cd260d75",
+    "url": "/static/css/main.dc9a6d8b.chunk.css"
+  },
+  {
+    "revision": "78b1f273767dbfb16a96",
+    "url": "/static/js/2.ef195b40.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/static/js/2.ef195b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a2b3ce8e4bdd75e665",
+    "url": "/static/js/3.d09e81aa.chunk.js"
+  },
+  {
+    "revision": "ea8b20392635cd260d75",
+    "url": "/static/js/main.ccdd6990.chunk.js"
+  },
+  {
+    "revision": "67d8f42a5b387f260672",
+    "url": "/static/js/runtime-main.99495fbf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "550273c58fce3055595c3f79b208558e",
+    "url": "/static/media/CSUG_group_photo.550273c5.jpeg"
+  },
+  {
+    "revision": "7c00141ffc58c671ffeb01396a2031b4",
+    "url": "/static/media/DHPhoto.7c00141f.jpg"
+  },
+  {
+    "revision": "008c62da4409844f69692d868edeec6c",
+    "url": "/static/media/DSCPhoto2.008c62da.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "849a51c66d6415410c53a13908e2d4f3",
+    "url": "/static/media/URLogo.849a51c6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "336a56a040eccc9f40bc70724191120d",
+    "url": "/static/media/fptLogo.336a56a0.png"
+  },
+  {
+    "revision": "74c84136adde747e29beac358b944299",
+    "url": "/static/media/greetingPhoto.74c84136.jpg"
+  },
+  {
+    "revision": "9271fad875af1cbdf2c6732802085d8f",
+    "url": "/static/media/react_native_logo.9271fad8.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "88296c7f8d969cab0db7e52af222f4c0",
+    "url": "/static/media/workPhoto.88296c7f.jpg"
+  }
+]);
